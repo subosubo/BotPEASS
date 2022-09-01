c1c8ed15fc127fddf54b89da15b03be8125107f4
subo subo <48369401+subosubo@users.noreply.github.com>
2022-09-01 11:23:03 +0000

    v0.1

--- keep_alive.py
+++ keep_alive.py
@@ -1,19 +1,15 @@
-#from threading import Thread
+from threading import Thread
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['HEAD','GET'])
 async def home():
-    #if request.method == 'HEAD':
-    #    await botpeas.main()
-        
     return 'Stayin Alive'
 
+def run():
+    app.run(host = "0.0.0.0", port=5000)
+
 def keep_alive():
-    from waitress import serve
-    serve(app, host = "0.0.0.0", port=5000, clear_untrusted_proxy_headers = False)
-
-
-if __name__ == "__main__":
-    keep_alive()
+    t = Thread(target=run)
+    t.start()
